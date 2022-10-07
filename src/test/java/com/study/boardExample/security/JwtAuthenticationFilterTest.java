0033f22ae5cefdcd654a9f9d2c6a2084d07a59e1
seoyoonho <gogoy2643@naver.com>
2022-10-07 10:00:38 +0900

    mokito 사용 spring security filter test case 추가

--- src/test/java/com/study/boardExample/security/JwtAuthenticationFilterTest.java
+++ src/test/java/com/study/boardExample/security/JwtAuthenticationFilterTest.java
@@ -1,21 +1,27 @@
 package com.study.boardExample.security;
 
+import com.study.boardExample.exception.JwtInvalidException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import java.io.IOException;
+import java.util.Collections;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.nullValue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -80,7 +86,42 @@ class JwtAuthenticationFilterTest {
     @Test
     public void givenThrowAuthenticationException_whenDoFilterInternal_thenSecurityContextInContextHolderIsNullAndClearContextBeenCalled() throws ServletException, IOException {
         //given
+        SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+        try (MockedStatic<SecurityContextHolder> utilties = Mockito.mockStatic(SecurityContextHolder.class)) {
+            utilties.when(SecurityContextHolder::getContext).thenReturn(securityContext);
+
+            mockHttpServletRequest.addHeader("Authorization", "Bearer valid_token");
+            JwtAuthenticationToken token = new JwtAuthenticationToken("valid_token");
+
+            when(mockAuthenticationManager.authenticate(token)).thenThrow(new JwtInvalidException("time expired"));
+
+            //when
+            jwtAuthenticationFilter.doFilterInternal(mockHttpServletRequest, mockHttpServletResponse, mockFilterChain);
+
+            //then
+            utilties.verify(SecurityContextHolder::clearContext, times(1));
+            assertThat(SecurityContextHolder.getContext().getAuthentication(), nullValue());
+        }
+    }
+
+    @Test
+    public void givenValidToken_whenDoFilterInternal_thenSecurityContextHasAuthentication() throws ServletException, IOException {
+        //given
+        mockHttpServletRequest.addHeader("Authorization", "Bearer valid_token");
+        JwtAuthenticationToken token = new JwtAuthenticationToken("valid_token");
+        JwtAuthenticationToken authenticationToken = new JwtAuthenticationToken(
+                "SeoYoonho",
+                "",
+                Collections.singletonList(() -> "ROLE_ADMIN")
+        );
+
+        when(mockAuthenticationManager.authenticate(token)).thenReturn(authenticationToken);
+
         //when
+        jwtAuthenticationFilter.doFilterInternal(mockHttpServletRequest, mockHttpServletResponse, mockFilterChain);
+
         //then
+        assertThat(SecurityContextHolder.getContext().getAuthentication(), equalTo(authenticationToken));
+
     }
 }
