296de23437af753911eabc324c045f414b8d70d0
seoyoonho <gogoy2643@naver.com>
2024-01-08 23:13:21 +0900

    커스텀 플러그인 적용 완료

--- src/main/java/com/study/boardExample/shinhan/qryfile/QryFileMethodAnnotation.java
+++ src/main/java/com/study/boardExample/shinhan/qryfile/QryFileMethodAnnotation.java
@@ -5,7 +5,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Retention(RetentionPolicy.RUNTIME)
+//@Retention(RetentionPolicy.RUNTIME)
+@Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.METHOD)
 public @interface QryFileMethodAnnotation {
     String qryFileCustomMethod() default "custom method";
